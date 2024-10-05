610f04138375c8a76b1e985cdb8fdeed0244aaab
mich0rtega <156623518+mich0rtega@users.noreply.github.com>
2024-10-04 21:14:40 -0600

    BACK END | Codificación Tablas\Categoria\Productos

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+#Tabla Categoria
+class Categoria(models.Model):
+    nombre = models.TextField(max_length=100)
+    referencia = models.CharField(max_length=8)
+#Tabla Productos
+class Producto(models.Model):
+    nombre = models.TextField(max_length=40)
+    descripcion = models.CharField(max_length=100)
+    referencia = models.CharField(max_length=8)
+    modelo = models.CharField(max_length=100)
+    marca = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10,decimal_places=2)
+    stock = models.IntegerField
+    #llave foranea
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+
