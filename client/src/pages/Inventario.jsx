d5aadb3387370d14bda86c28059f9976f7a79ec6
EDWIN ALFREDO SOLORZA OCHOA <edwinsolorza3@gmail.com>
2024-10-11 13:50:50 -0600

    FrontEnd | Inventario Finish

--- client/src/pages/Inventario.jsx
+++ client/src/pages/Inventario.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Dashboard from '../components/Dashboard';
 import '../Styles/Estilodash.css';
+import CardProd from '../components/CardProd';
 
 function Inventario() {
   return (
     <>
       <Dashboard/>
+      <CardProd/>
+
     </> 
   );
       
