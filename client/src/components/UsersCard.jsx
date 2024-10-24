89ad93df8fa1f0844f24d7bc20432584e3bef0d6
Gerardo Ontiveros <gontiverosdavila01@gmail.com>
2024-10-24 12:05:19 -0600

    BACKEND | AGREGAR FUNCIONALIDAD A TARJETAS DE LA PAGINA HOME

--- client/src/components/UsersCard.jsx
+++ client/src/components/UsersCard.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import api from "@/lib/api";
 
 function UsersCard() {
@@ -6,6 +7,8 @@ function UsersCard() {
   const [roles, setRoles] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  const employeesCard = 5;
 
   useEffect(() => {
     const fetchUsuarios = async () => {
@@ -45,38 +48,51 @@ function UsersCard() {
 
   const currentUserId = localStorage.getItem("id");
 
+  // Mezclar usuarios aleatoriamente y seleccionar 4
+  const shuffledUsers = usuarios
+    .filter((user) => user.id !== currentUserId)
+    .sort(() => Math.random() - 0.5)
+    .slice(0, employeesCard);
+
   const rolesMap = roles.reduce((acc, rol) => {
     acc[rol.id_rol] = rol.nombre_rol;
     return acc;
   }, {});
+
   return (
     <section className="bg-white w-[291px] h-[540px] rounded-xl shadow-lg ml-[30px] px-6 py-4">
-      <h5 className="text-center text-[18px] font-bold text-black/70 ">
-        Trabajores Activos
-      </h5>
+      <div className="flex flex-row justify-between items-center ">
+        <h5 className="text-center text-[18px] font-bold text-black/70 ">
+          Empleados
+        </h5>
+        <Link
+          to="/Empleados"
+          className="text-[14px] mr-6 text-black/70 font-semibold hover:text-[#045E9C]"
+        >
+          Ver todos
+        </Link>
+      </div>
       <div className="flow-root">
         <ul role="list" className="divide-y divide-gray-200">
-          {usuarios
-            .filter((user) => user.id !== currentUserId)
-            .map((user) => (
-              <li key={user.id} className="py-3">
-                <div className="flex items-center space-x-4">
-                  <img
-                    className="w-8 h-8 rounded-full"
-                    src={user.user_profile_image}
-                    alt={`${user.username}'s perfil`}
-                  />
-                  <div className="flex-1 min-w-0">
-                    <p className="text-sm font-medium text-gray-900 truncate">
-                      {user.username}
-                    </p>
-                    <p className="text-sm text-gray-500 truncate">
-                      {rolesMap[user.rol]}
-                    </p>
-                  </div>
+          {shuffledUsers.map((user) => (
+            <li key={user.id} className="py-3">
+              <div className="flex items-center space-x-4">
+                <img
+                  className="w-16 h-16 rounded-full"
+                  src={user.user_profile_image}
+                  alt={`${user.username}'s perfil`}
+                />
+                <div className="flex-1 min-w-0">
+                  <p className="text-sm font-medium text-black truncate">
+                    {user.first_name} {user.last_name}
+                  </p>
+                  <p className="text-sm text-gray-500 truncate">
+                    {rolesMap[user.rol]}
+                  </p>
                 </div>
-              </li>
-            ))}
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </section>
