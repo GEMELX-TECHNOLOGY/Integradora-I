c2fd735749692a3ce44741a3235b0d318cc53ff6
Gerardo Ontiveros <gontiverosdavila01@gmail.com>
2024-10-24 21:39:07 -0600

    BACKENG | ADD FUNCIONALITY TO ESTADISTIC GRAPHIC

--- client/src/components/Estadistic.jsx
+++ client/src/components/Estadistic.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
+import api from "@/lib/api"
+
 
 function Estadistic() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const query = await api.get("api/ventas/");
+        const res = await query.data
+
+        const shuffled = res.sort(() => 0.5 - Math.random());
+
+        const randomMasVendidos = shuffled.slice(0, 5);
+        setData(randomMasVendidos);
+        console.log(randomMasVendidos)
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const shortenName = (referencia, maxLength = 10) => {
+    return referencia.length > maxLength ? referencia.slice(0, maxLength) + "..." : referencia;
+  };
+
+
   return (
-    <section className='w-[757px] bg-white h-[540px] shadow-lg rounded-xl px-6 py-4 ml-11'>
-        <h5 className='text-center text-[18px] font-bold text-black/70 '>Ventas</h5>
-        <div></div>
+    <section className="w-[1190px] bg-white h-[540px] shadow-lg rounded-xl px-6 py-4 ml-11">
+      <h5 className="text-center text-[18px] font-bold text-black/70">
+        Ventas
+      </h5>
+      <div className="flex items-center justify-center">
+        <LineChart
+          width={1190}
+          height={500}
+          data={data}
+          margin={{ top: 5, right: 2, bottom: 5, left: 0 }}
+        >
+          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="referencia" tickFormatter={(name) => shortenName(name)} />
+          <YAxis />
+        </LineChart>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Estadistic
+export default Estadistic;
