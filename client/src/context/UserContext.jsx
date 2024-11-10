02c037c9ef7ac62b724e7bb6e09eb0ea315e42ad
Gerardo Ontiveros <gontiverosdavila01@gmail.com>
2024-11-09 21:06:06 -0600

    ADD FUNCTIONS TO USERCONTEXT TO VERIFY USER ROLE

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,22 +1,52 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from "react";
+import api from "@/lib/api";
 
-// Crear el contexto
-export const UserContext = createContext();
+const UserContext = createContext();
 
-// Crear el proveedor del contexto
 export const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null);
-    const [userRole, setUserRole] = useState("");
-
-    // Función para actualizar los datos del usuario
-    const loginUser = (userData) => {
-        setUser(userData);
-        setUserRole(userData.rol);
+    const [loading, setLoading] = useState(true);
+  
+    const fetchUserDetails = async (token) => {
+      try {
+        const response = await api.get("/api/v1/user/", {
+          headers: { Authorization: `Token ${token}` },
+        });
+        setUser(response.data);
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
     };
-
+  
+    const loginUser = (token, userDetails) => {
+      localStorage.setItem("access", token);
+      setUser(userDetails);  // Establece el usuario completo
+    };
+  
+    const resetUser = () => {
+      setUser(null);
+      setLoading(false);
+    };
+  
+    useEffect(() => {
+      const token = localStorage.getItem("access");
+      if (token) {
+        fetchUserDetails(token);
+      } else {
+        setUser(null);
+        setLoading(false);
+      }
+    }, []);
+  
     return (
-        <UserContext.Provider value={{ user, userRole, loginUser }}>
-            {children}
-        </UserContext.Provider>
+      <UserContext.Provider value={{ user, loading, loginUser, resetUser }}>
+        {children}
+      </UserContext.Provider>
     );
-};
+  };
+  
+
+export const useUser = () => useContext(UserContext);
