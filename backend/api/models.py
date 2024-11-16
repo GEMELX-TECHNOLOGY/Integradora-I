77287cc1ab1f67fb9e18750ab7a48312b9d7802d
Gerardo Ontiveros <gontiverosdavila01@gmail.com>
2024-11-15 18:06:30 -0600

    ADD EMPLOYEE MODEL

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,6 +3,44 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from .manager import UserManager
 
+################ - USUARIOS - ################
+#Tabla Rol
+class Rol(models.Model):
+    id_rol = models.AutoField(primary_key=True)
+    nombre_rol = models.CharField(max_length=60, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nombre_rol
+#Tabla Usuarios
+class Usuarios(AbstractUser):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    user_profile_image = models.ImageField(upload_to='Perfil/', blank=True, null=True)
+    rol = models.ForeignKey(Rol, on_delete=models.CASCADE, default=1)
+
+    USERNAME_FIELD = 'username'
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
+    
+    @property
+    def is_active(self):
+        return self.rol.is_active
+
+    @property
+    def is_staff(self):
+        return self.rol.is_staff
+
+    @property
+    def is_superuser(self):
+        return self.rol.is_superuser
+
 ################## INVENTARIO ###################
 #Tabla Proveedor
 class Proveedor(models.Model):
@@ -46,48 +84,38 @@ class Producto(models.Model):
         return self.nombre
     
 
-################ - USUARIOS - ################
-#Tabla Rol
-class Rol(models.Model):
-    id_rol = models.AutoField(primary_key=True)
-    nombre_rol = models.CharField(max_length=60, unique=True)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
+############## RH ##################
+#Tabla Nomina
+class Nomina(models.Model):
+    id_nom = models.AutoField(primary_key=True)
+    fecha_pago = models.DateField()
+    salario_base = models.DecimalField(max_digits=10,decimal_places=2)
+    bonos = models.DecimalField(max_digits=10,decimal_places=2)
+    salario_nto = models.DecimalField(max_digits=10,decimal_places=2)
+    usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.nombre_rol
-
-#Tabla Usuarios
-class Usuarios(AbstractUser):
-    id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=20, unique=True)
-    password = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    user_profile_image = models.ImageField(upload_to='Perfil/', blank=True, null=True)
-    rol = models.ForeignKey(Rol, on_delete=models.CASCADE, default=1)
-
-    USERNAME_FIELD = 'username'
-    objects = UserManager()
-
-    def __str__(self):
-        return self.username
-    
-    @property
-    def is_active(self):
-        return self.rol.is_active
-
-    @property
-    def is_staff(self):
-        return self.rol.is_staff
-
-    @property
-    def is_superuser(self):
-        return self.rol.is_superuser
+#Tabla Horario
+class Horario(models.Model):
+  id_horario = models.AutoField(primary_key=True)
+  dia_semana = models.TextField()
+  hora_entrada = models.TimeField()
+  hora_salida = models.TimeField()
+  turno = models.CharField(max_length=100, choices=[('Matutino', 'Matutino'), ('Vespertino', 'Vespertino')])
 
 
-
-    
+class Empleados(models.Model):
+    nombre = models.CharField(max_length=255)
+    apellido_pa = models.CharField(max_length=255)
+    apellido_ma= models.CharField(max_length=255)
+    rfc = models.CharField(max_length=13, unique=True, default="")
+    calle = models.CharField(max_length=60, default="")
+    numero_ext = models.CharField(max_length=10, default="")
+    numero_int =  models.CharField(max_length=10, default="" )
+    cod_Postal = models.CharField(max_length=5, default="")
+    estado = models.CharField(max_length=60, default="")
+    pais = models.CharField(max_length=60, default="")
+    usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    horario = models.ForeignKey(Horario, on_delete=models.CASCADE, default="")
 
 ################## VENTAS #################
 #Tabla Clientes
@@ -145,24 +173,6 @@ class Devoluciones(models.Model):
     venta = models.ForeignKey(Ventas, on_delete=models.CASCADE)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
 
-############## RH ##################
-#Tabla Nomina
-class Nomina(models.Model):
-    id_nom = models.AutoField(primary_key=True)
-    fecha_pago = models.DateField()
-    salario_base = models.DecimalField(max_digits=10,decimal_places=2)
-    bonos = models.DecimalField(max_digits=10,decimal_places=2)
-    salario_nto = models.DecimalField(max_digits=10,decimal_places=2)
-    usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
-
-#Tabla Horario
-class Horario(models.Model):
-  id_horario = models.AutoField(primary_key=True)
-  dia_semana = models.TextField()
-  hora_entrada = models.TimeField()
-  hora_salida = models.TimeField()
-  turno = models.CharField(max_length=100, choices=[('Matutino', 'Matutino'), ('Vespertino', 'Vespertino')])
-  usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
 
 ###########################################
 class ChatMessage(models.Model):
